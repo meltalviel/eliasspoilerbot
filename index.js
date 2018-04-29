e0c3e70b62ab85be0eded7e7e1506643326b10b4
meltalviel <38821851+meltalviel@users.noreply.github.com>
2018-04-28 21:32:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const Discord = require("discord.js");
-const SpoilerBot = require('discord-spoiler-bot');
 const client = new Discord.Client();
 
 let config = {
-    token: 'NDM5NTM3MzMxMDI5MDE2NTgw.DcUm6g.ZJ2yrFEZ-qOhyRCgl7D9Q72hRRQ',
     markAllowAll: true,
     maxLines: 16,
 };
