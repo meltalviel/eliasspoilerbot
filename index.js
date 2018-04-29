0c42367be4d810daa0a6f8e10cc559ca0993849a
meltalviel <38821851+meltalviel@users.noreply.github.com>
2018-04-28 21:21:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Discord = require("discord.js");
+const SpoilerBot = require('discord-spoiler-bot');
+
+let config = {
+    token: 'NDM5NTM3MzMxMDI5MDE2NTgw.DcUm6g.ZJ2yrFEZ-qOhyRCgl7D9Q72hRRQ',
+    markAllowAll: true,
+    maxLines: 16,
+};
+
+let bot = new SpoilerBot(config,{disableEveryone: true});
+
+bot.connect("ready", async () => {
+});
